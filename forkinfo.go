aa164d652691bb0aa06a2a8e062a4d188dfb1d08
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-05-20 04:01:12 +0100

    Handle invalid user/repo argument
    Moves usage info and aborting into its own function to make it easier to
    have more specific error handling without writing the same `fmt.Println`
    lines for every single different error we might encounter.

--- forkinfo.go
+++ forkinfo.go
@@ -3,17 +3,30 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
 )
 
 func main() {
-    if len(os.Args[1:]) != 1 {
-        fmt.Println("Not enough or too many arguments supplied.")
-        fmt.Println()
-        fmt.Println("Usage: " + os.Args[0] + " <user>/<repository>")
-        os.Exit(1)
+    if len(os.Args[1:]) == 0 {
+        abort("Not enough arguments supplied.")
+    }
+
+    if len(os.Args[1:]) > 1 {
+        abort("Too many arguments supplied.")
     }
 
     repo := os.Args[1]
 
+    if ! strings.Contains(repo, "/") {
+        abort("Argument is not a valid user/repo string.")
+    }
+
     fmt.Println("Repository set to " + repo)
+}
+
+func abort(msg string) {
+    fmt.Println("[ERROR] " + msg)
+    fmt.Println()
+    fmt.Println("Usage: " + os.Args[0] + " <user>/<repository>")
+    os.Exit(1)
 }
