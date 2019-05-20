d100f1212e56d53a84093d610948979c6adaca0e
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-05-20 05:36:08 +0100

    Add rudimentary repository fetching
    Splits the input argument into an array on the '/' and further puts the
    values from that array into `username` and `repository` variables
    respectively.
    
    Also imports Google's go-github package and uses that to fetch repo data
    based on the given username/repository, and bails if it can't be found.
    For now we just spit out formatted JSON with MarshalIndent, mostly
    because `fmt.Printf("%v", json)` is ugly and totally unreadable.
    
    Can't reassign variables in Go so instead of assigning `os.Args[1]` to
    `repo` we just use it directly so that `repo` is freed up for the actual
    repository data returned from the Github API.

--- forkinfo.go
+++ forkinfo.go
@@ -1,27 +1,45 @@
 package main
 
 import (
+    "context"
+    "encoding/json"
     "fmt"
     "os"
     "strings"
+
+    "github.com/google/go-github/github"
 )
+
+func fetchRepository(username, repository string) (*github.Repository, error) {
+    client := github.NewClient(nil)
+    data, _, err := client.Repositories.Get(context.Background(), username, repository)
+
+    return data, err
+}
 
 func main() {
     if len(os.Args[1:]) == 0 {
         abort("Not enough arguments supplied.")
     }
-
     if len(os.Args[1:]) > 1 {
         abort("Too many arguments supplied.")
     }
-
-    repo := os.Args[1]
-
-    if ! strings.Contains(repo, "/") {
+    if ! strings.Contains(os.Args[1], "/") {
         abort("Argument is not a valid user/repo string.")
     }
 
-    fmt.Println("Repository set to " + repo)
+    args := strings.Split(os.Args[1], "/")
+    username, repository := args[0], args[1]
+
+    fmt.Println("Attempting to fetch repository...")
+    repo, err := fetchRepository(username, repository)
+    if (err != nil) {
+        fmt.Printf("[ERROR] %v\n", err)
+        os.Exit(1)
+    }
+
+    json, err := json.MarshalIndent(repo, "", "  ")
+    fmt.Print(string(json))
 }
 
 func abort(msg string) {
